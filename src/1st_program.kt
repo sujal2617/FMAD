3663a4dbab8438ba764603d1d44c0c5516e9ea3c
sujal2617 <2200032771@kluniversity.in>
2024-07-29 12:21:38 +0530

    fmad uplode

---
+++ src/1st_program.kt
@@ -0,0 +1,34 @@
+fun value(a:Int):Int{
+    val a: Int=80
+    println(a)
+    return a+30
+}
+fun surname(h:String):String{
+    val s:String="Pattewar"
+    return h+s
+}
+fun main() {
+//     val mynum :Double=26.24
+
+
+     val b: Int=20
+     println(b)
+     println(value(10))
+
+
+//     val name:String ="sujal "
+
+
+//     println(name)
+//     print("sujal ")
+
+
+//     print(surname(name))
+
+//     val name=arrayOf("1","2","3","4")
+//     name[3]="7"
+//     println(name[1])
+
+
+
+}
